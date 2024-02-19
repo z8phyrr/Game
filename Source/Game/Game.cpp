c1344e426f81228d0e62d4c4a734d468879262e6
z8phyrr <zephyrpoirier@gmail.com>
2024-02-18 19:08:45 -0500

    :3
    init commit

---
+++ Source/Game/Game.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game, "Game" );
+
+DEFINE_LOG_CATEGORY(LogGame)
+ 
